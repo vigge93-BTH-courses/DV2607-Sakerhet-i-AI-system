3022ed245c14c082890245d0a256062e711146c5
blueflamefirez <blueflamefirez@gmail.com>
2022-11-07 12:12:25 +0100

    Fixed some pep8 issues

--- Project/all_CNN.py
+++ Project/all_CNN.py
@@ -7,13 +7,11 @@ class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
 
-
-
-
 if __name__ == "__main__":
     data = d.getCifar10()
     train_data = data['train_data']['data']
     train_labels = data['train_data']['labels']
+
     plt.figure(figsize=(10, 10))
     for i in range(25):
         plt.subplot(5, 5, i + 1)
@@ -21,7 +19,7 @@ if __name__ == "__main__":
         plt.yticks([])
         plt.grid(False)
         plt.imshow(train_data[i])
-        # The CIFAR labels happen to be arrays, 
+        # The CIFAR labels happen to be arrays,
         # which is why you need the extra index
         plt.xlabel(class_names[train_labels[i][0]])
     plt.show()
