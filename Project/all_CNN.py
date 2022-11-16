de7d918c1c30131032b24f54cc613a848f824022
blueflamefirez <blueflamefirez@gmail.com>
2022-11-16 11:31:34 +0100

    Pre-final CNN

--- Project/all_CNN.py
+++ Project/all_CNN.py
@@ -1,25 +1,41 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
+import tensorflow_addons as tfa
+import matplotlib as plt
 from keras import layers, models
 import data as d
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
+
+def get_cnn(summary=False):
+    model = models.Sequential()
+    model.add(layers.Conv2D(96, kernel_size=3, strides=1, padding='same', activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.Conv2D(96, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(layers.Conv2D(96, kernel_size=3, strides=2, padding='same', activation='relu'))
+    model.add(layers.Conv2D(192, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(layers.Conv2D(192, kernel_size=3, strides=1, padding='same', activation='relu'))
+    model.add(layers.Dropout(0.3))
+    model.add(layers.Conv2D(192, kernel_size=3, strides=2, padding='same', activation='relu'))
+    model.add(layers.Conv2D(192, kernel_size=3, strides=2, padding='same', activation='relu'))
+    model.add(layers.Conv2D(192, kernel_size=1, strides=1, padding='valid', activation='relu'))
+    model.add(layers.Conv2D(10, kernel_size=1, strides=1, padding='valid', activation='relu'))
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Flatten())
+    model.add(layers.Activation('softmax'))
+
+    if summary is True:
+        model.summary()
+    return model
+
+
+def compile_fit(model, x, y, x_test, y_test, epochs=100, batch_size=128):
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy', tfa.metrics.F1Score(num_classes=10)])
+    history = model.fit(x, y, epochs=epochs, validation_data=(x_test, y_test), batch_size=batch_size)
+    return history
 
 
 if __name__ == "__main__":
-    data = d.getCifar10()
-    train_data = data['train_data']['data']
-    train_labels = data['train_data']['labels']
-
-    plt.figure(figsize=(10, 10))
-    for i in range(25):
-        plt.subplot(5, 5, i + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(train_data[i])
-        # The CIFAR labels happen to be arrays,
-        # which is why you need the extra index
-        plt.xlabel(class_names[train_labels[i][0]])
-    plt.show()
+    train_data, train_labels, test_data, test_labels = d.getCifar10()
+    model = get_cnn(summary=True)
+    history = compile_fit(model, train_data, train_labels, test_data, test_labels, epochs=10, batch_size=128)
+    # plt.plot(history.history['accuracy'], label='accuracy')
